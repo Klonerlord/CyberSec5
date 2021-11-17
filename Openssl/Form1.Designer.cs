771966a7d1a685a7f9ac267d355be479c0d7fd70
unknown <Svalera2000@gmail.com>
2021-11-17 10:19:49 +0200

    Code upload

---
+++ Openssl/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Openssl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.RichTextBox();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.vector = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(240, 426);
+            this.input.TabIndex = 0;
+            this.input.Text = "";
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.Location = new System.Drawing.Point(548, 12);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(240, 426);
+            this.output.TabIndex = 1;
+            this.output.Text = "";
+            // 
+            // encrypt
+            // 
+            this.encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypt.Location = new System.Drawing.Point(351, 250);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(97, 36);
+            this.encrypt.TabIndex = 2;
+            this.encrypt.Text = "Encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decrypt.Location = new System.Drawing.Point(351, 292);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(97, 36);
+            this.decrypt.TabIndex = 3;
+            this.decrypt.Text = "Decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // vector
+            // 
+            this.vector.Location = new System.Drawing.Point(297, 188);
+            this.vector.Name = "vector";
+            this.vector.Size = new System.Drawing.Size(214, 20);
+            this.vector.TabIndex = 4;
+            this.vector.TextChanged += new System.EventHandler(this.vector_TextChanged);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(297, 142);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(214, 20);
+            this.key.TabIndex = 5;
+            this.key.TextChanged += new System.EventHandler(this.key_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(294, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(294, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Init vector";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.vector);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "OpenSSL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox input;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.TextBox vector;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Openssl/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Openssl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            output.SelectionFont = new Font("Microsoft Sans Serif", 8);
+            key.Text = "crypto1234crypto";
+            vector.Text = "thermoAAAAthermo";
+            input.Text = "7IwDEpdhsP41iKPnRGEWe1HUMASC3nPY8OGvfwCBmeG1rK2drdhEQG1nK0B" +
+                "HcQVXXpQZ/iICZ51My1tvB6BEcEewJaArY1K2eVsS1KwIdap9YZrPlKRKZloW/jjgJTGD" +
+                "5v3FPANDlXygiMSWdtLNTDwm1pQ9an3ko3q21p2PSAD3B2f/++KfYhjxcoAjqFMhsbINh" +
+                "pbLtmTMjFQgB45T+62VnSE1yYprP/1iFd7ejfUg+wtZzkT+Dxl1joL198x+QmLHzSxh2Z" +
+                "iXlA1kwJcyjoQwZVIKAUC47H1nvR2ZZN0tPhA5LwU0YxQENHcN/3aERi7ayFRmHweCE9Y" +
+                "WIALeHjTGg80MOzo+6+vM7jgYWjilBGBpT/KNRgBldX0XYy31e3nA8lHNmgN/D23zbTV1" +
+                "TF1oV2L0FL1FpjmfDJtNbVuheZJ2MlnnhcP/BlO2rwrmGjrC9j7m/g8LZZh7PaBjiYPjL" +
+                "Ou3Ji0rQtu3FuJM3Z8/XvLYLUYKn2rJLuCg2/KAJr+YmMzlg15cpnul7MKfaomHT9salC" +
+                "dKgra29Tu+BDDFTJ+wWyUjXwXqZzIqpZQG2Bks9UTITq5ng/IFmrmTzH1M8EIOAKPGSMR" +
+                "XpqkAZPhaIRWZAdYvYeflp57dnWjKMimRmENvn7BQjDAyGdB9G6wJIT5nTft6aebmPhY3" +
+                "J5nIgFxeV7Eo2Am3jGtvc8L0o/1Qcpdxu7N5itPwFSdrks1q7YnMBkADlAGLxROt00H2t" +
+                "jQID7D9cLG1PkE3JOiVD9rhrkYbZutKAZ9C7sMw6IUZCarcSjT5gTUms1/IFJ3VCMlU31" +
+                "eC739tzpVQRm8eom3ppW8F4pbUAJ3wdcLOPKtWvn/3781ezgYDmEDPSrXMTzOOvY4Hz+Y" +
+                "ZfcavsKoeq6SNzztdwBHR00mF7kA7pThSyYzGZqHK0yAB9kDcDQTm0MyOPDxSHyv2M8iJ" +
+                "+abnA5RrBgfqBeOJjJL6bP0jZZG5qfxEzA7bNhTri1W/oU3gH1lCGvrrrUIeiDKbfli2V" +
+                "g7OR+7oC964gCRDjhP9iu/cB3TAOTesIVsnqmuhvsrF2F7cG660Vv3tVmO2YjIEPcMjPM" +
+                "qvXYk2x5GLfvZIwerLrMJrcimOciDLTLJaiyrkIkJTgOcliAizQU7KK8g3ruXige7CFFx" +
+                "3egPPn1AodHOCzxhSIKmBk4B9wXDGzJuzpV7NxNEU8jfYP3EEkI5qcjrsdkMxJ/xZIRbk" +
+                "YwulAqdED4vbEfq/zg13LQB6jkWmTo1S//+AMvilhv/WgIaqtLttz6cAlooDgMLlSYT6H" +
+                "GGkBSNA639vQFlfONoy4DDl7xz7f7a8qpPMR2A2TGOz8wI/nZ17/ZatzwRnudD8dlikwy" +
+                "yQNZIdAosLNfC+nj74wUCaoIdiDiFjDoph7ADAxR/t4+jgIhqh7Q1nod9nKqHctrbYNQN" +
+                "zDRAIn9WXtVsg1zYiMYuH7/aTo+wl/7pXBz8UWDP9tqtjvUJZwQ0rvwbWfTU8aA==";
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            Chilkat.Crypt2 encrypt = new Chilkat.Crypt2();
+            encrypt.CryptAlgorithm = "aes";
+            encrypt.CipherMode = "ctr";
+            encrypt.KeyLength = 128;
+            encrypt.EncodingMode = "base64";
+            encrypt.SetEncodedIV(vector.Text, "ascii");
+            encrypt.SetEncodedKey(key.Text, "ascii");
+            output.Text = encrypt.EncryptStringENC(input.Text);
+        }
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            Chilkat.Crypt2 decrypt = new Chilkat.Crypt2();
+            decrypt.CryptAlgorithm = "aes";
+            decrypt.CipherMode = "ctr";
+            decrypt.KeyLength = 128;
+            decrypt.EncodingMode = "base64";
+            decrypt.SetEncodedIV(vector.Text, "ascii");
+            decrypt.SetEncodedKey(key.Text, "ascii");
+            output.Text = decrypt.DecryptStringENC(input.Text);
+        }
+
+        private void key_TextChanged(object sender, EventArgs e)
+        {
+            if (vector.Text.ToCharArray().Length != 16 || key.Text.ToCharArray().Length != 16)
+            { encrypt.Enabled = false; decrypt.Enabled = false; }
+            else
+            { encrypt.Enabled = true; decrypt.Enabled = true; }
+        }
+
+        private void vector_TextChanged(object sender, EventArgs e)
+        {
+            if (vector.Text.ToCharArray().Length != 16 || key.Text.ToCharArray().Length != 16)
+            { encrypt.Enabled = false; decrypt.Enabled = false; }
+            else
+            { encrypt.Enabled = true; decrypt.Enabled = true; }
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            input.Font = new Font("Microsoft Sans Serif", 8);
+        }
+    }
+}
